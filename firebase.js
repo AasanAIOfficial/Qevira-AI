11e7770c4bc5ff04f023cf039b1910d2d345ea04
Aasan AI <aasanaiai2026@gmail.com>
2026-07-24 13:37:53 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
-
+import { GoogleGenAI } from "https://esm.run/@google/genai";
 const firebaseConfig = {
   apiKey: "AIzaSyAUulx-T2-Crx3B6iTbBUSFkX7TeR4CTRY",
   authDomain: "qevira-ai.firebaseapp.com",
